a0b41641786360b09c8329e063cf85ec23b95611
Fran Asir <Fran Asir>
2016-10-03 13:20:00 +0200

    Hecho

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,7 +1,9 @@
 public class HolaMundo{
 	//Mi Nombre
-	public static void main(String args[]){
-		System.out.println("Francisco Elena Puente,Edad: 20, Nacimiento: León");
+	public static void main(String [] args){
+		System.out.println("Francisco Elena Puente");
+		System.out.println("Edad: 20");
+		System.out.println("Nacimiento: León");
 
 	}
 }
